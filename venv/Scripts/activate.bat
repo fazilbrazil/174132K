12b5472eb3a33d9461e234fd80635ef7738b4b13
ASUS <174132K@mymail.nyp.edu.sg>
2018-01-12 09:50:14 +0800

    Initial commit

---
+++ venv/Scripts/activate.bat
@@ -0,0 +1,32 @@
+@echo off
+set "VIRTUAL_ENV=D:\Fazil\NYP\NYP_Year_1\Semester_2_Y1\OOP\174132K\venv"
+
+if not defined PROMPT (
+    set "PROMPT=$P$G"
+)
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+)
+
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(venv) %PROMPT%"
+
+if defined PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+    set PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
